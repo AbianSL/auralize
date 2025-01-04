0432e19cd7509d04c28dad14ca332e1227af082e
AbianSL <abisanle@gmail.com>
2025-01-04 20:40:04 +0000

    check if the audios was downloaded

--- convert.py
+++ convert.py
@@ -1,12 +1,20 @@
 import audio_file_names as afn
-from io. 
+from io.input import Audio 
+
 from pathlib import Path
+import os
 
 # local paths
 DOWNLOAD_DIR = Path("esc50_data")
-AUDIO_DIR = DOWNLOAD_DIR / "spectrograms"
+AUDIO_DIR = DOWNLOAD_DIR / "audio"
 METADATA_FILE = DOWNLOAD_DIR / "esc50.csv"
+SPEC_DIR = DOWNLOAD_DIR / "spectrograms"
 
-# create directories if they don't exist
-AUDIO_DIR.mkdir(parents=True, exist_ok=True)
+# detect if the audio directory exists
+if not os.path.exists(AUDIO_DIR) or not os.path.isdir(AUDIO_DIR):
+    print("The directory does not exist. Execute download.py first.")
+    exit()
+
+# create the spectrogram directory
+SPEC_DIR.mkdir(parents=True, exist_ok=True)
 
