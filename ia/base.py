4babba31d4c893907c3510d99261fca165f2838a
AbianSL <abisanle@gmail.com>
2025-01-14 19:34:23 +0000

    libriries to trian the model

--- ia/base.py
+++ ia/base.py
@@ -8,6 +8,11 @@ import os
 import tensorflow as tf
 import pandas as pd
 from keras.preprocessing import image
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Flatten, Dense
 
 class SpectrogramTrainer:
     def __init__(self, spectrogram_dir: Path,
@@ -24,6 +29,7 @@ class SpectrogramTrainer:
         self.model_dir = model_dir
         self.spectrograms = []
         self.labels = []
+        self.model = Sequential() 
         self._load_data()
 
     def _load_data(self):
@@ -33,16 +39,38 @@ class SpectrogramTrainer:
         self._load_spectrograms()
         self._load_labels()
 
+    def train(self):
+        """
+            Train the model
+        """
+        # layer 1
+        self.model.add(Conv2D(32, (3, 3), input_shape=(224, 224, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        # layer 2
+        self.model.add(Conv2D(32, (3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        # layer 3
+        self.model.add(Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        # layer 4
+        self.model.add(Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(units=128, activation='relu'))
+        self.model.add(Dense(units=1, activation='sigmoid'))
+        self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        self.model.summary()
+
+        print("Training the model")
+        # TODO: how to train the model
+        # self.model.fit(self.spectrograms, self.labels, epochs=10, batch_size=32)
+
     def _load_spectrograms(self):
         """
             Load spectrograms
         """
-        images = []
-        labels = []
-        for file in os.listdir(self.spectrogram_dir):
-            images.append(image.img_to_array(image.load_img(os.path.join(self.spectrogram_dir, file), target_size=(224, 224, 3))))
-            labels.append((label))
-        return images, labels
+        pass 
 
     def _load_labels(self):
         """
