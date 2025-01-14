d8fca067a48d92f9ab68e87d82ede7fd11f2be2b
EliasHernandezAbreu <alu0101487137@ull.edu.es>
2025-01-14 19:35:41 +0000

    making single file pipe

--- auralize.py
+++ auralize.py
@@ -1,5 +1,18 @@
+import numpy as np
 import tensorflow as tf
 
+from pathlib import Path
+from sys import argv
+from audio.input import Audio
+
 if __name__ == "__main__":
+    if len(argv) != 2:
+        exit(1)
+    file_path = Path(argv[1])
+    audio = Audio(file_path)
+    audio.save_spectrogram(Path("spectrogram.png"));
+    exit(0)
+
     model = tf.keras.models.load_model("auralize_model.keras", None, True)
-    model.predict
+    spectrogram = np.load("spectrogram.png");
+    prediction = model.predict(spectrogram);
